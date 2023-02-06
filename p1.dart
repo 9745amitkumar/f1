fe4019b3ee1d32bf22986d06e9a8dadbcc1c4ec2
9745amitkumar <59292236+9745amitkumar@users.noreply.github.com>
2023-02-07 01:49:52 +0530

    Add files via upload

---
+++ p1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void main() {
+runApp(MaterialApp(
+home: Scaffold(
+appBar: AppBar(
+title: Text("Scaffold usage WITH SAFE AREA"),
+),
+body: Container(
+child: SafeArea(
+child: Center(
+child: Text(
+'This is scaffold area',style: TextStyle(
+fontWeight: FontWeight.bold,
+),
+),
+),
+),
+),
+),
+),);
+}
